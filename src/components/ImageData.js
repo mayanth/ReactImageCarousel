84e5de2342cf864efa8c1dbbdd86f7494b608862
mayanth <anothai.ssht@gmail.com>
2022-10-11 14:18:55 +0700

    Image Carousel

---
+++ src/components/ImageData.js
@@ -0,0 +1,16 @@
+const ImageData = [
+    {
+        title:"เที่ยวทะเล",
+        image:"https://cdn.pixabay.com/photo/2017/01/20/00/30/maldives-1993704_960_720.jpg"
+    },
+    {
+        title:"ดอกทานตะวัน",
+        image:"https://cdn.pixabay.com/photo/2021/02/12/09/36/sunflowers-6007847_960_720.jpg"
+    },
+    {
+        title:"ผีเสื้อ",
+        image:"https://cdn.pixabay.com/photo/2014/05/26/13/32/butterfly-354528_960_720.jpg"
+    },
+]
+
+export default ImageData
